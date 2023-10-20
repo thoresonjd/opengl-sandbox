83e205784247cc15f31e1fce4f3658d73dee0d42
thoresonjd <thoresonjd@gmail.com>
2023-10-20 13:07:57 -0700

    Updated naming conventions in camera.h

--- include/opengl-sandbox/camera.h
+++ include/opengl-sandbox/camera.h
@@ -15,11 +15,14 @@
  */
 class Camera {
 private:
-
+	static constexpr float MIN_PITCH = -89.99f;
+	static constexpr float MAX_PITCH = 89.99f;
+	static constexpr float MIN_FOV = 1.0f;
+	static constexpr float MAX_FOV = 45.0f;
 	static constexpr float DEFAULT_PITCH = 0.0f;
 	static constexpr float DEFAULT_YAW = -90.0f;
 	static constexpr float DEFAULT_MOVEMENT_SPEED = 5.0f;
-	static constexpr float DEFAULT_MOUSE_SENSITIVITY = 0.1f;
+	static constexpr float DEFAULT_LOOK_SENSITIVITY = 0.1f;
 	static constexpr float DEFAULT_FOV = 45.0f;
 	glm::vec3 initialPosition;
 	glm::vec3 position;
@@ -30,7 +33,7 @@ private:
 	float pitch;
 	float yaw;
 	float movementSpeed;
-	float mouseSensitivity;
+	float lookSensitivity;
 	float fieldOfView;
 	
 	/**
@@ -63,25 +66,28 @@ public:
 	~Camera() = default;
 	
 	/**
-	 * Processes keyboard input
+	 * Processes changes in the position the camera is located.
+	 * Movement can be conducted via keyboard input.
 	 * @param direction - The direction in which to move the camera
 	 * @param deltaTime - The change in time to apply to movement velocity
 	 */
-	void processKeyboard(Movement direction, float deltaTime);
+	void move(Movement direction, float deltaTime);
 
 	/**
-	 * Processes mouse movement
-	 * @param offsetX - Amount in which the mouse has moved along the x-axis
-	 * @param offsetY - Amount in which the mouse has moved along the y-axis
+	 * Processes changes in the direction in which the camera is looking.
+	 * Looking can be adjusted via moving the mouse.
+	 * @param offsetX - Amount in which the camera has looked along the x-axis
+	 * @param offsetY - Amount in which the camera has looked along the y-axis
 	 * @param constrainPitch - Determines if the pitch should be constrained within a safe range
 	 */
-	void processMouseMovement(float offsetX, float offsetY, bool constrainPitch = true);
+	void look(float offsetX, float offsetY, bool constrainPitch = true);
 
 	/**
-	 * Processes mouse scroll
-	 * @param offsetY - Amount in which the mouse has been scrolled
+	 * Processes changes in field of view.
+	 * Field of view can be adjusted via mouse scroll.
+	 * @param offset - Amount in which the field of view has changed
 	 */
-	void processMouseScroll(float offsetY);
+	void adjustFOV(float offset);
 	
 	/**
 	 * Computes the view matrix for the camera given it's position, front, and up vectors
@@ -125,7 +131,7 @@ Camera::Camera(
 	pitch(pitch),
 	yaw(yaw),
 	movementSpeed(DEFAULT_MOVEMENT_SPEED),
-	mouseSensitivity(DEFAULT_MOUSE_SENSITIVITY),
+	lookSensitivity(DEFAULT_LOOK_SENSITIVITY),
 	fieldOfView(DEFAULT_FOV) {
 	updateCameraVectors();
 }
@@ -142,12 +148,12 @@ Camera::Camera(
 	pitch(pitch),
 	yaw(yaw),
 	movementSpeed(DEFAULT_MOVEMENT_SPEED),
-	mouseSensitivity(DEFAULT_MOUSE_SENSITIVITY),
+	lookSensitivity(DEFAULT_LOOK_SENSITIVITY),
 	fieldOfView(DEFAULT_FOV) {
 	updateCameraVectors();
 }
 
-void Camera::processKeyboard(Movement direction, float deltaTime) {
+void Camera::move(Movement direction, float deltaTime) {
 	float velocity = movementSpeed * deltaTime;
 	switch (direction) {
 		case Movement::FORWARD:
@@ -165,26 +171,26 @@ void Camera::processKeyboard(Movement direction, float deltaTime) {
 	}
 }
 
-void Camera::processMouseMovement(float offsetX, float offsetY, bool constrainPitch) {
-	offsetX *= mouseSensitivity;
-	offsetY *= mouseSensitivity;
+void Camera::look(float offsetX, float offsetY, bool constrainPitch) {
+	offsetX *= lookSensitivity;
+	offsetY *= lookSensitivity;
 	yaw += offsetX;
 	pitch += offsetY;
 	if (constrainPitch) {
-		if (pitch > 89.9f)
-			pitch = 89.9f;
-		else if (pitch < -89.9f)
-			pitch = -89.9f;
+		if (pitch > MAX_PITCH)
+			pitch = MAX_PITCH;
+		else if (pitch < MIN_PITCH)
+			pitch = MIN_PITCH;
 	}
 	updateCameraVectors();
 }
 
-void Camera::processMouseScroll(float offsetY) {
-	fieldOfView -= offsetY;
-	if (fieldOfView < 1.0f)
-		fieldOfView = 1.0f;
-	else if (fieldOfView > 45.0f)
-		fieldOfView = 45.0f;
+void Camera::adjustFOV(float offset) {
+	fieldOfView -= offset;
+	if (fieldOfView < MIN_FOV)
+		fieldOfView = MIN_FOV;
+	else if (fieldOfView > MAX_FOV)
+		fieldOfView = MAX_FOV;
 }
 
 glm::mat4 Camera::getViewMatrix() const {
